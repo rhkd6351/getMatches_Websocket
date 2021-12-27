620b96270b26979244023d34662296b28ab47f37
Lim_Yekwang <56215323+rhkd6351@users.noreply.github.com>
2021-12-27 15:38:54 +0900

    change socket.js

--- socket.js
+++ socket.js
@@ -14,8 +14,10 @@ module.exports = (server, app) => {
 
     const req = socket.request;
     const id = socket.id;
+
     let roomID = socket.handshake.query.roomID;
     let name = socket.handshake.query.name;
+    let isPart = socket.handshake.query.isPart;
     let headOfTeam = false;
     let clientsCount =
       room.adapter.rooms.get(roomID) && room.adapter.rooms.get(roomID).size;
@@ -27,52 +29,67 @@ module.exports = (server, app) => {
     let response = {
       msg: "",
       date: new Date(),
-      user: id,
+      user: name,
     };
 
-    if (clientsCount && clientsCount >= 2) {
-      console.log("최대 참여 가능 인원을 초과하였습니다");
-      response.msg = "인원을 초과하였습니다";
+    //참가자는 참여코드가 유효한지 확인 (참여자 숫자로 구분)
+    if (isPart && isPart == true) {
+      response.msg = "참여 코드를 확인해주세요.";
       socket.emit("error", response);
       socket.disconnect();
     } else {
-      if (!clientsCount) headOfTeam = true;
+      headOfTeam = true;
+    }
+
+    //인원초과 여부 확인
+    if (clientsCount && clientsCount >= 2) {
+      response.msg =
+        "입장 가능한 인원이 초과되었습니다. \n다른 방을 만들어주세요.";
+      socket.emit("error", response);
+      socket.disconnect();
+    } else {
+      // if (!clientsCount) headOfTeam = true;
       socket.join(roomID);
-      console.log(roomID);
-      response.msg = name + "님이 참여하였습니다";
-      io.of("room").to(roomID).emit("msg", response);
+
+      response.msg = `초대링크를 공유해서 친구들을 초대해주세요! 참여코드: ${roomID}`;
+      io.of("room").to(roomID).emit("system", response);
+
+      response.msg = name + "님이 입장하셨습니다.";
+      io.of("room").to(roomID).emit("system", response);
     }
 
     socket.on("disconnect", () => {
-      console.log("room 네임스페이스 접속 해제");
-      response.msg = id + "님이 퇴장하였습니다";
-      io.of("room").to(roomID).emit("msg", response);
+      response.date = new Date();
+      response.msg = name + "님이 퇴장하였습니다";
+      io.of("room").to(roomID).emit("system", response);
       socket.leave(roomID);
     });
 
-    socket.on("startGame", (data) => {
+    socket.on("start", (data) => {
       response.date = new Date();
-      response.data = "game start";
-      io.of("room").to(roomID).emit("startGame", response);
-    });
-
-    socket.on("endGame", (data) => {
-      response.date = new Date();
-      response.data = "game end";
-      io.of("room").to(roomID).emit("endGame", response);
+      response.data = data;
+      io.of("room").to(roomID).emit("start", response);
     });
 
     socket.on("pick", (data) => {
       response.date = new Date();
       response.data = data.matchNum;
-      response.remainMatches = data.remainMatches;
+      // response.remainMatches = data.remainMatches;
       io.of("room").to(roomID).emit("pick", response);
     });
 
+    //클라이언트에서 토글방식으로 동작
     socket.on("endTurn", (data) => {
       response.date = new Date();
-      response.data = "end turn";
+      response.data = data;
       io.of("room").to(roomID).emit("endTurn", response);
+    });
+
+    socket.on("endGame", (data) => {
+      response.date = new Date();
+      // response.data = "game end";
+      io.of("room").to(roomID).emit("system", "게임이 종료되었습니다.");
+      // io.of("room").to(roomID).emit("endGame", response);
     });
 
     socket.on("msg", (data) => {
